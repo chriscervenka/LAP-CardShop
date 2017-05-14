843f5914ed840a996a7d20fc2f2b638047f6f6db
chriscervenka <christiancervenka2@gmail.com>
2017-05-14 17:12:12 +0200

    Last

--- CardGameLap/CardGame/CardGame.Web/App_Start/BundleConfig.cs
+++ CardGameLap/CardGame/CardGame.Web/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace CardGame.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/toastr").Include(
+                        "~/Scripts/js/toastr.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,7 +28,10 @@ namespace CardGame.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/jquery.jscrollpane.custom.css",
+                      "~/Content/custom.css",
+                      "~/Content/bookblock.css",
+                      "~/Content/MyStyle.css"));
         }
     }
 }
