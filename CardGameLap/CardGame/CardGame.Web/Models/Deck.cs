7691adc6aea141e499faa175de65166e0f162dc8
chriscervenka <christiancervenka2@gmail.com>
2017-05-27 22:08:21 +0200

    Model DECK erstellt

---
+++ CardGameLap/CardGame/CardGame.Web/Models/Deck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CardGame.Web.Models
+{
+    public class Deck
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Card> Cards { get; set; }
+    }
+}
